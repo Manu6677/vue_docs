788e2ccdfdf5f87a29a41897eca1552ec7e87d69
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-10-26 15:27:12 +0530

    v-model-clear-capital-btn

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,7 +3,35 @@ export default {
   data() {
     return {
       message: "",
+      date: true,
     };
+  },
+  created() {
+    console.log("created");
+    this.date = new Date();
+  },
+
+  mounted() {
+    console.log("mounted");
+    this.date = new Date();
+  },
+
+  updated() {
+    console.log("updated");
+    // this.date = new Date();
+  },
+
+  destroyed() {
+    console.log("destroyed");
+    this.date = new Date();
+  },
+  methods: {
+    clear() {
+      this.message = " ";
+    },
+    capitalLetter() {
+      this.message = this.message.toUpperCase();
+    },
   },
 };
 </script>
@@ -11,7 +39,14 @@ export default {
 <template>
   <div>
     <h3>Type here Dude</h3>
-    <input v-model="message" placeholder="type here" />
+    <span>
+      <input v-model="message" placeholder="type here" />
+      -- <span><button @click="clear">Clear</button></span> --<span>
+        <button @click="capitalLetter()">Make in capital</button></span
+      >
+    </span>
     <p>{{ message }}</p>
+
+    <p>{{ date }}</p>
   </div>
 </template>
