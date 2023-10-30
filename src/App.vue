a1a70892508007bbb3a7834c4244a707866baa64
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-10-30 14:09:57 +0530

    namedslotsIteration

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
-<script setup></script>
+<script>
+import FancyList from "./components/FancyList.vue";
 
-<template></template>
+export default {
+  components: {
+    FancyList,
+  },
 
-<style scoped></style>
+  data() {
+    return {
+      bookData: [
+        { body: "Scoped Slots Guide", username: "Evan You", likes: 20 },
+        { body: "Vue Tutorial", username: "Natalia Tepluhina", likes: 10 },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <FancyList v-for="(item, index) in bookData" :key="index">
+    <template #nameUser>
+      {{ item.username }}
+    </template>
+
+    <template #books>
+      {{ item.body }}
+    </template>
+
+    <template #likes>
+      {{ item.likes }}
+    </template>
+  </FancyList>
+</template>
+
+<style scoped>
+.meta {
+  font-size: 0.8em;
+  color: #42b883;
+}
+</style>
---
+++ src/components/FancyList.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: ["api-url", "per-page"],
+  data() {
+    return {
+      items: [],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <slot name="nameUser" />, <slot name="books" />,
+      <slot name="likes" />
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 5px;
+  background: linear-gradient(315deg, #42d392 25%, #647eff);
+}
+li {
+  padding: 5px 20px;
+  margin: 10px;
+  background: #fff;
+}
+</style>
