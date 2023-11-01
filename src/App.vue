c0a0dde9aaa0e1ce5ceff599843a4c98d5146f57
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-11-01 11:27:16 +0530

    card-compo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
-<script setup></script>
+<script>
+import Cards from "./components/Cards.vue";
+import forest from "./assets/forest.jpg"
+export default{
+    components: {Cards},
+    data(){
+        return {
 
-<template></template>
+        }
+    },
+}
+
+</script>
+
+<template>
+<div class="card"> 
+<Cards>
+<template #header>
+  <!-- imgcame -->
+  <img src="https://keepwalestidy.cymru/wp-content/uploads/2021/10/arnaud-mesureur-7EqQ1s3wIAI-unsplash-555x480.jpg" alt="NoImg"/>
+</template>
+
+<template #title>
+<h3>Card Title</h3>
+</template>
+
+<template #body>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
+</template>
+
+<template #footer>
+<button>Don't click</button>
+</template>
+
+</Cards>
+</div>
+</template>
 
 <style scoped></style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,60 @@
+<script> </script>
+
+<template> 
+<div class="container"> 
+<div class="inside-container">
+
+<header class="img">
+<slot name="header"/>
+</header>
+
+<main class="card-title">
+<slot name="title"/>
+</main>
+
+<body class="body"> 
+<slot name="body"/>
+</body>
+
+<footer class="footer"> 
+<slot name="footer" class="btn"/>
+</footer>
+
+</div>
+</div>
+</template>
+
+<style scoped> 
+.container{
+    margin-top: 160px;
+    margin-left: 480px;
+    justify-content: center;
+    border: 1px solid black;
+    border-radius: 20px;
+    height: 400px;
+    width: 450px;
+    background-color:#C8C8C8;
+}
+
+.inside-container{
+    margin-left: 30px;
+   margin-top: 20px;
+}
+
+.card-title{
+    margin-top: 10px
+}
+
+.body{
+    height: 90px;
+}
+
+.img{
+    width: 400px;   
+   height: 180px;
+   object-fit: contain;
+   overflow: hidden;
+   border-radius: 20px;
+}
+
+</style>
